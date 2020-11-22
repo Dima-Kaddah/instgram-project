e704ade8a068ffd5ed3ba300bdb80b656d33a7b9
Dima <dimakaddah@gmail.com>
2020-11-22 21:45:52 +0100

    add login signup using materializecss

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Login = () => {
 
   return (
-    <h1>Login</h1>
+    <div className='my-card'>
+      <div className='card auth-card input-field'>
+        <h2>Instagram</h2>
+        <input type='email' placeholder='email'></input>
+        <input type='password' placeholder='password'></input>
+        <button className='btn waves-effect waves-light #64b5f6 blue lighten-2'>Login</button>
+      </div>
+    </div >
   );
 };
 export default Login;
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -3,7 +3,15 @@ import React from 'react';
 const Signup = () => {
 
   return (
-    <h1>Signup</h1>
+    <div className='my-card'>
+      <div className='card auth-card input-field'>
+        <h2>Instagram</h2>
+        <input type='text' placeholder='name'></input>
+        <input type='email' placeholder='email'></input>
+        <input type='password' placeholder='password'></input>
+        <button className='btn waves-effect waves-light #64b5f6 blue lighten-2'>Signup</button>
+      </div>
+    </div >
   );
 };
 export default Signup;
