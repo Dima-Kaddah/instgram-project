994dc7ca51c66d41a1a03b083503f84faf0ad0dc
Dima <dimakaddah@gmail.com>
2020-11-23 01:57:28 +0100

    add profile client

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -3,7 +3,36 @@ import React from 'react';
 const Profile = () => {
 
   return (
-    <h1>Profile</h1>
+    <div className='pro'>
+      <div className='flexIt'>
+        <div>
+          <img className='profileImg' src='https://images.unsplash.com/photo-1592446660312-150a57429e79?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTU2fHxwZXJzb258ZW58MHwyfDB8&auto=format&fit=crop&w=800&q=60' /></div>
+        <div>
+          <h4>Danila Gomez</h4>
+          <div className='flexContent'>
+            <h6>20 posts</h6>
+            <h6>162 followers</h6>
+            <h6>168 following</h6>
+          </div>
+        </div>
+      </div>
+
+      <div className='gallery'>
+        <img className='item' src='https://images.unsplash.com/photo-1586299485759-f62264d6b63f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTU3fHxwZXJzb258ZW58MHwyfDB8&auto=format&fit=crop&w=800&q=60' />
+        <img className='item' src='https://images.unsplash.com/photo-1586299485759-f62264d6b63f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTU3fHxwZXJzb258ZW58MHwyfDB8&auto=format&fit=crop&w=800&q=60' />
+        <img className='item' src='https://images.unsplash.com/photo-1586299485759-f62264d6b63f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTU3fHxwZXJzb258ZW58MHwyfDB8&auto=format&fit=crop&w=800&q=60' />
+        <img className='item' src='https://images.unsplash.com/photo-1586299485759-f62264d6b63f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTU3fHxwZXJzb258ZW58MHwyfDB8&auto=format&fit=crop&w=800&q=60' />
+        <img className='item' src='https://images.unsplash.com/photo-1586299485759-f62264d6b63f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTU3fHxwZXJzb258ZW58MHwyfDB8&auto=format&fit=crop&w=800&q=60' />
+        <img className='item' src='https://images.unsplash.com/photo-1586299485759-f62264d6b63f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTU3fHxwZXJzb258ZW58MHwyfDB8&auto=format&fit=crop&w=800&q=60' />
+        <img className='item' src='https://images.unsplash.com/photo-1586299485759-f62264d6b63f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTU3fHxwZXJzb258ZW58MHwyfDB8&auto=format&fit=crop&w=800&q=60' />
+        <img className='item' src='https://images.unsplash.com/photo-1586299485759-f62264d6b63f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTU3fHxwZXJzb258ZW58MHwyfDB8&auto=format&fit=crop&w=800&q=60' />
+        <img className='item' src='https://images.unsplash.com/photo-1586299485759-f62264d6b63f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTU3fHxwZXJzb258ZW58MHwyfDB8&auto=format&fit=crop&w=800&q=60' />
+        <img className='item' src='https://images.unsplash.com/photo-1586299485759-f62264d6b63f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTU3fHxwZXJzb258ZW58MHwyfDB8&auto=format&fit=crop&w=800&q=60' />
+        <img className='item' src='https://images.unsplash.com/photo-1586299485759-f62264d6b63f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTU3fHxwZXJzb258ZW58MHwyfDB8&auto=format&fit=crop&w=800&q=60' />
+
+      </div>
+
+    </div>
   );
 };
 export default Profile;
