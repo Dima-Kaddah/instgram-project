7bc4216e5de6106b2bd19be0283a56c04d2f6fb2
Dima <dimakaddah@gmail.com>
2020-11-25 20:35:19 +0100

    add handleErrors middleware to app.js

--- app.js
+++ app.js
@@ -28,6 +28,15 @@ app.use((req, res, next) => {
 app.use('/api', userRoute);
 app.use('/api', postRoute);
 
+//handleErrors
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
+});
+
 //server listen
 const port = process.env.PORT || 5000;
 const server = () => {
