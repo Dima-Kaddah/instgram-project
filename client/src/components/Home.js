091cacfbff4513b475de084af88467dd590b3590
Dima <dimakaddah@gmail.com>
2020-12-06 22:01:55 +0100

    make the get allposts in home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,27 +1,18 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { useParams } from 'react-router-dom';
 import useHttpClient from './../hooks/http-hook';
-import AuthContext from '../shared/Auth-context.js';
 
 const Home = () => {
-  const [post, setPosts] = useState();
-  const { postId } = useParams();
-
-  const auth = useContext(AuthContext);
+  const [posts, setPosts] = useState([]);
   const { isLoading, sendRequest } = useHttpClient();
 
   useEffect(() => {
     const fetchPost = async () => {
-      const request = {
-        headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + auth.token },
-      };
-
       try {
         const responseData = await sendRequest(
-          `${process.env.REACT_APP_BACKEND_URL}/allposts`
-          , request.headers
+          `${process.env.REACT_APP_BACKEND_URL}/allposts`,
         );
-        setPosts(responseData);
+        setPosts(responseData.posts);
+        console.log(responseData.posts);
       } catch (err) { }
     };
     fetchPost();
@@ -29,18 +20,23 @@ const Home = () => {
 
   return (
     <div className='home'>
-      <div className='card home-card'>
-        <h5>Danila</h5>
-        <div className='card-image'>
-          <img src='https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbHBhcGVyfGVufDB8MHwwfA%3D%3D&auto=format&fit=crop&w=800&q=60' />
-        </div>
-        <div className='card-content'>
-          <i className='material-icons' style={{ color: 'red' }}>favorite</i>
-          <h6>title</h6>
-          <p>this the post </p>
-          <input type='text' placeholder='add a comment' />
-        </div>
-      </div>
+      { isLoading && <div>Loading...</div>}
+      {!isLoading && posts && posts.map(post => {
+        return (
+          <div className='card home-card'>
+            <h5>{post.postedBy.name}</h5>
+            <div className='card-image'>
+              <img src={post.image} />
+            </div>
+            <div className='card-content'>
+              <i className='material-icons' style={{ color: 'red' }}>favorite</i>
+              <h6>{post.title}</h6>
+              <p>{post.description}</p>
+              <input type='text' placeholder='add a comment' />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
